dc5909d1c77d239607c6d1d45594b1a57cec4957
lijianan <574980606@qq.com>
2022-03-13 19:23:06 +0800

    fix

--- src/promise.ts
+++ src/promise.ts
@@ -13,8 +13,8 @@ const isFunc = (value: any): value is Function => typeof value === 'function';
 class MyPromise {
   PromiseResult: any;
   PromiseState: State;
-  onFulfilledCallbacks: ResolveType[] = [];
-  onRejectedCallbacks: RejectType[] = [];
+  private onFulfilledCallbacks: ResolveType[] = [];
+  private onRejectedCallbacks: RejectType[] = [];
   constructor(func: ConstructorFn) {
     this.PromiseState = State.PENDING;
     this.PromiseResult = null;
@@ -26,7 +26,7 @@ class MyPromise {
       this.reject(err);
     }
   }
-  resolve = (result: any) => {
+  private resolve = (result: any) => {
     if (this.PromiseState === State.PENDING) {
       setTimeout(() => {
         this.PromiseState = State.FULFILLED;
@@ -37,7 +37,7 @@ class MyPromise {
       });
     }
   };
-  reject = (reason: any) => {
+  private reject = (reason: any) => {
     if (this.PromiseState === State.PENDING) {
       setTimeout(() => {
         this.PromiseState = State.REJECTED;
@@ -48,7 +48,7 @@ class MyPromise {
       });
     }
   };
-  then = (onFulfilled?: ResolveType, onRejected?: RejectType) => {
+  public then = (onFulfilled?: ResolveType, onRejected?: RejectType) => {
     onFulfilled = isFunc(onFulfilled)
       ? onFulfilled
       : value => {
@@ -107,15 +107,8 @@ class MyPromise {
   };
 }
 
-/**
- * 对resolve()、reject() 进行改造增强 针对resolve()和reject()中不同值情况 进行处理
- * @param  {promise} promise2 promise1.then方法返回的新的promise对象
- * @param  {[type]} x         promise1中onFulfilled或onRejected的返回值
- * @param  {[type]} resolve   promise2的resolve方法
- * @param  {[type]} reject    promise2的reject方法
- */
-function resolvePromise(promise2: any, x: any, resolve?: ResolveType, reject?: RejectType) {
-  if (x === promise2) {
+function resolvePromise(promise2: MyPromise, x: any, resolve?: ResolveType, reject?: RejectType) {
+  if (promise2 === x) {
     return reject?.(new TypeError('Chaining cycle detected for promise'));
   }
   if (x instanceof MyPromise) {
@@ -131,7 +124,6 @@ function resolvePromise(promise2: any, x: any, resolve?: ResolveType, reject?: R
       reject?.(x.PromiseResult);
     }
   } else if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
-    // 2.3.3 如果 x 为对象或函数
     let then;
     try {
       then = x.then;
@@ -139,12 +131,10 @@ function resolvePromise(promise2: any, x: any, resolve?: ResolveType, reject?: R
       return reject?.(e);
     }
     if (typeof then === 'function') {
-      // 2.3.3.3.3 如果 resolvePromise 和 rejectPromise 均被调用，或者被同一参数调用了多次，则优先采用首次调用并忽略剩下的调用
-      let called = false; // 避免多次调用
+      let called = false;
       try {
         then.call(
           x,
-          // 2.3.3.3.1 如果 resolvePromise 以值 y 为参数被调用，则运行 [[Resolve]](promise, y)
           (y: any) => {
             if (called) {
               return;
@@ -152,7 +142,6 @@ function resolvePromise(promise2: any, x: any, resolve?: ResolveType, reject?: R
             called = true;
             resolvePromise(promise2, y, resolve, reject);
           },
-          // 2.3.3.3.2 如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
           (r: any) => {
             if (called) {
               return;
@@ -166,17 +155,12 @@ function resolvePromise(promise2: any, x: any, resolve?: ResolveType, reject?: R
           return;
         }
         called = true;
-        /**
-         * 2.3.3.3.4.2 否则以 e 为据因拒绝 promise
-         */
         reject?.(e);
       }
     } else {
-      // 2.3.3.4 如果 then 不是函数，以 x 为参数执行 promise
       resolve?.(x);
     }
   } else {
-    // 2.3.4 如果 x 不为对象或者函数，以 x 为参数执行 promise
     return resolve?.(x);
   }
 }
